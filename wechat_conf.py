be0c6d62684daf1407e349ede095b0087afbff6a
John Doe <john@sample.com>
2018-11-24 13:32:49 +0800

    1.0

---
+++ wechat_conf.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+
+# 此为企业的ID号
+CorpID = 'wwbd9b074d49559d1e'
+
+# 应用的ID
+Agentid = 1000004
+
+# 认证信息，企业ID+认证信息可获取tokent，获取之后向此tokent发送内容
+Secret = 'xehSTs-KHtUWU9GCEF3EXGVL0nWsLY7W2RLX4Sr5Irg'
